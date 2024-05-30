18c7445aec140cf53dc817140ee29da5357836c0
Elise Medeiros <79584555+Elise-16-medeiros@users.noreply.github.com>
2024-05-29 22:02:19 -0300

    feat: tailwind configuration

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -3,7 +3,8 @@
 export default function Home() {
   return (
     <main>
-      <h1>Hello</h1>
+      <h1 className="font-title text-3xl">Hello world</h1>
+      <p className="font-body">Lorem ipsum dolor sit amet.</p>
     </main>
   );
 }
--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -1,13 +1,14 @@
+import { title } from "process";
 import type { Config } from "tailwindcss"
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -18,6 +19,10 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        body: ["var(--font-body)"],
+        title: ["var(--font-title)"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -52,6 +57,33 @@ const config = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        green: {
+          50: "#30AF5B",
+          90: "#292C27",
+        },
+        gray: {
+          10: "#EEEEEE",
+          20: "#A2A2A2",
+          30: "#7B7B7B",
+          50: "#585858",
+          90: "#141414",
+        },
+        orange: {
+          50: "#FF814C",
+        },
+        blue: {
+          70: "#021639",
+        },
+        yellow: {
+          50: "#FEC601",
+        },
+      },
+      backgroundImage: {
+        "bg-img-1": "url('/img-1.png')",
+        "bg-img-2": "url('/img-2.png')",
+        "feature-bg": "url('/feature-bg.png')",
+        pattern: "url('/pattern.png')",
+        "pattern-2": "url('/pattern-bg.png')",
       },
       borderRadius: {
         lg: "var(--radius)",
@@ -72,9 +104,18 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+
+      screens: {
+        xs: "400px",
+        "3xl": "1680px",
+        "4xl": "2200px",
+      },
+      maxWidth: {
+        "10xl": "1512px",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
 export default config
